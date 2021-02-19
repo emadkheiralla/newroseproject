39e8ea71134082fea9e936d72e396e1b1ab807c7
emadkheiralla <emadkheiralla@hotmail.com>
2021-02-19 00:12:21 -0500

    Initial push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'new-rose-project';
+  isSubmitted = false;
+  Dogs = [{id: 1, breed: 'Beagle'}, {id: 2, breed: 'Pitbull'}, {id: 3, breed: 'Terrier'}, {id: 4, breed: 'Rottweiler'}]
+
+  constructor(public fb: FormBuilder) { }
+
+  /*########### Form ###########*/
+  myForm = this.fb.group({
+    dogName: ['', [Validators.required]]
+  })
+
+  // Getter method to access formcontrols
+  get dog() {
+    return this.myForm.get('dogName');
+  }
+
+  /*########### Template Driven Form ###########*/
+  onSubmit() {
+    this.isSubmitted = true;
+    if (this.myForm.invalid) {
+      return false;
+    } else {
+      alert(JSON.stringify(this.myForm.value))
+    }
+
+  }
 }
