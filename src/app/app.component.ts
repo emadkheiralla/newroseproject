f780b0ca6c42f1b251c3f24084e67c94f2e715c9
emadkheiralla <emadkheiralla@hotmail.com>
2021-02-20 22:27:04 -0500

    updated with angular material table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from "@angular/forms";
+import {MatTable} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
@@ -9,21 +10,48 @@ import { FormBuilder, Validators } from "@angular/forms";
 export class AppComponent{
   title = 'new-rose-project';
   isSubmitted = false;
-  Dogs = [{id: 1, breed: 'Beagle'}, {id: 2, breed: 'Pitbull'}, {id: 3, breed: 'Terrier'}, {id: 4, breed: 'Rottweiler'}]
-
+  Dogs = [{name: '001', compound: 'cp1', mechanism: 'mec1', breed: 'Beagle'}, {name: '002', compound: 'cp2', mechanism: 'mec2', breed: 'Pitbull'}, {name: '003', compound: 'cp3', mechanism: 'mec3', breed: 'Terrier'}, {name: '004', compound: 'cp4', mechanism: 'mec4', breed: 'Beagle'}]
+  chosenDogs = [];
+  @ViewChild(MatTable) table: MatTable<any>;
   constructor(public fb: FormBuilder) { }
 
   /*########### Form ###########*/
   myForm = this.fb.group({
-    dogName: ['', [Validators.required]]
+    compound: [''],
+    mechanism: ['']
   })
-
+  displayedColumns: string[] = ['name', 'compound', 'mechanism', 'breed' ];
   // Getter method to access formcontrols
-  get dog() {
-    return this.myForm.get('dogName');
+  get dogCompound() {
+    return this.myForm.get('compound');
   }
 
-  /*########### Template Driven Form ###########*/
+  get dogMechanism() {
+    return this.myForm.get('mechanism');
+  }
+
+  onChange(event) {
+    this.chosenDogs = [];
+    if(this.dogCompound.value){
+      this.chosenDogs.push(this.dogCompound.value)
+    }
+    if(this.dogMechanism.value){
+      this.chosenDogs.push(this.dogMechanism.value)
+    }
+    
+    // if(event.target.name === 'compound'){
+    //   this.chosenDogs.push(this.dogCompound.value)
+    // } 
+    
+    // if(event.target.name === 'mechanism'){
+    //   this.chosenDogs.push(this.dogMechanism.value)
+    // }
+    if(this.table){
+      this.table.renderRows();
+    }
+    
+  }
+
   onSubmit() {
     this.isSubmitted = true;
     if (this.myForm.invalid) {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table'  
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatTableModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
